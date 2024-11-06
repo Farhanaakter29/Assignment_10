70e5b8907bb551a99a0223608c98744e70633081
farhana akter <farhanaakter120223@gmail.com>
2024-11-06 12:52:57 +0600

    prblm 3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new MyRunnable("Thread 1"));
-        Thread thread2 = new Thread(new MyRunnable("Thread 2"));
-        Thread thread3 = new Thread(new MyRunnable("Thread 3"));
+        Thread thread1 = new Thread(new MyRunnable(), "Thread-A");
+        Thread thread2 = new Thread(new MyRunnable(), "Thread-B");
+        Thread thread3 = new Thread(new MyRunnable(), "Thread-C");
         thread1.start();
         thread2.start();
         thread3.start();
--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -1,16 +1,7 @@
 class MyRunnable implements Runnable {
-    private String threadName;
-    public MyRunnable(String name) {
-        this.threadName = name;
-    }
     @Override
     public void run() {
-        try {
-            System.out.println(threadName + " is running.");
-            Thread.sleep(3000);
-            System.out.println(threadName + " has woken up.");
-        } catch (InterruptedException e) {
-            System.out.println(threadName + " was interrupted.");
-        }
+        // Print the name of the current thread
+        System.out.println("Current thread: " + Thread.currentThread().getName());
     }
 }
